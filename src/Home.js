04833128dc72d4500f7e102e0f90ba96bcbd49c3
Aditya Sunil Wakale <adityawakale208@gmail.com>
2024-07-21 17:02:22 +0530

    NameList from letter a

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import PrimeChecker from './components/PrimeChecker';
+import NameList from './components/NameList';
 
+let data = [
+  {name: 'aditya'}, {name: 'aniket'}, {name: 'swapnil'}, {name: 'chiku'}, {name: 'sakshi'}, {name: 'shital'}
+];
 
 export default function Home() {  
   return (
    <>
    <div className='container'>
-    <h3>Write a react com that given a number prop nd display wether number is prime or not</h3>
+    <h3>Write a react component that given a number prop nd display wether number is prime or not</h3>
     <PrimeChecker number={11}/>
-    {/* <PrimeNumber n={3}/> */}
+    </div>
+   <div className='container'>
+    <h3>Write a component that receives an array of objects with name property. Render alphabetically orders lits containing only the names that start with lett 'C'</h3>
+    <NameList data={data} />
     </div>
     </>
   );
---
+++ src/components/NameList.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function NameList({data}) {
+
+const filterdData = data.filter(element => element.name.startsWith('a')).map(element => element.name).sort();
+  return (
+    <div>
+    <h2>Names starting with 'S'</h2>
+    <ul>
+      {filterdData.map((name, index) => (
+        <li key={index}>{name}</li>
+      ))}
+    </ul>
+  </div>
+  )
+}
